8d6c6dd4f0688f4571d5907c98a4992c8d953a2b
PALLE PRANAY REDDY <88979290+pallepranay@users.noreply.github.com>
2023-01-07 12:32:21 +0530

    Update 286_missingNumber.cpp

--- 286_missingNumber.cpp
+++ 286_missingNumber.cpp
@@ -1,3 +1,5 @@
+// Approach 1 : Using Bit Manipulation: 
+// Best Method
 // Time Complexity: O(N) Space Complexity: O(1)
 class Solution {
 public:
@@ -11,5 +13,25 @@ public:
         return res;
 
         
+    }
+};
+
+// Approach 2 : Difference of Sum of the numbers:
+// Methond is not preffered in interviews
+// Time Complexity: O(N) Space Complexity: O(1)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int arrSum = 0;
+        for (int i:nums){
+            arrSum+=i;
+        }
+
+        int n = nums.size();
+        int totSum = n*(n+1)/2;
+
+        return totSum - arrSum;
+
     }
 };
